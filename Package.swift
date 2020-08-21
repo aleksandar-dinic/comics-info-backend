9ce6b040d4009aa4782089c7e836d993ef6fdd57
Aleksandar Dinic <a.dinic@icloud.com>
2020-08-21 16:35:06 +0200

    Add AWS dependencies

--- Package.swift
+++ Package.swift
@@ -5,16 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "ComicsInfoBackend",
+    platforms: [
+        .macOS(.v10_14),
+    ],
+    products: [
+         .executable(
+            name: "ComicsInfoBackend",
+            targets: ["ComicsInfoBackend"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", .upToNextMajor(from:"0.2.0")),
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift.git", from: "5.0.0-alpha.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ComicsInfoBackend",
-            dependencies: []),
+            dependencies: [
+                .product(name: "AWSDynamoDB", package: "aws-sdk-swift"),
+                .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
+                .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime")
+            ]),
         .testTarget(
             name: "ComicsInfoBackendTests",
             dependencies: ["ComicsInfoBackend"]),
